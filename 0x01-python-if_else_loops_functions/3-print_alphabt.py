c9d9246f41bff8a797dedb0a2b38f661ccb64be4
mobisa-012 <mobisakwamboka@gmail.com>
2022-06-01 07:43:49 +0300

    fix

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for a in range(ord('a'), ord('z') + 1):
-    if a != ord('q') and a != ord('e'):
-        print('{:c}'.format(a), end="")
+
+for i in range(0, 26):
+    if i not in (4, 16):
+        print("{:c}".format(i + 97), end="")
