479d825c653a94583a45bc96d39a075c885e1655
mobisa-012 <mobisakwamboka@gmail.com>
2022-06-03 17:21:41 -0700

    fix

--- 0x02-python-import_modules/3-infinite_add.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    total = 0
-    for a in range(1, len(sys.argv)):
-        total += int(sys.argv[a])
-    print("{}".format(total))
--- 0x02-python-import_modules/README.md
+++
@@ -1,1 +0,0 @@
- Day 3 of python - import & modules
