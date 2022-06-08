98d1d5ded9d95efae9aa67a77b8b086566d28d32
mobisa-012 <mobisakwamboka@gmail.com>
2022-06-08 12:24:12 -0700

    removed

--- 0x04-python-more_data_structures/102-complex_delete.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def complex_delete(a_dictionary, value):
-    keys = list(a_dictionary.keys())
-    for v in keys:
-        if a_dictionary.get(v) == value:
-            del a_dictionary[v]
-    return a_dictionary
