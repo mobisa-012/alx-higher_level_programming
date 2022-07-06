2d091479bf33b4c222f808a4002970738603d017
mobisa-012 <mobisakwamboka@gmail.com>
2022-07-05 21:45:24 -0700

    corrections

--- 0x0B-python-input_output/2-append_write.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""holbertontask"""
-
-
-def append_write(filename="", text=""):
-    """holbertontask"""
-    with open(filename, "a", encoding="utf-8") as f:
-        return f.write(text)
