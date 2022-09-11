8ef2974543653789f01a8a00e04efbe7237b1c5d
Bina Mobisa <mobisakwamboka@gmail.com>
2022-09-11 15:52:20 +0300

    fix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,32 +1,24 @@
 #!/usr/bin/python3
-""" module that lists all states in hbtn_0e_0_usa databse """
+"""
+ con
+"""
+import MySQLdb
+import sys
 
-if __name__ == '__main__':
-    # Standard Library imports
-    import sys
 
-    # related third party imports
-    import MySQLdb as sql
+def mainx():
+    """con"""
 
-    user = sys.argv[1]
-    passwd = sys.argv[2]
-    database = sys.argv[3]
-
-    conn = sql.connect(
-            host='localhost',
-            port=3306,
-            user=user,
-            passwd=passwd,
-            db=database)
-
-    cur = conn.cursor()
-
-    cur.execute("SELECT * FROM states ORDER BY id")
-
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3]
+                         )
+    cur = db.cursor()
+    cur.execute('SELECT * FROM states ORDER BY id ASC')
     rows = cur.fetchall()
-
     for row in rows:
         print(row)
-
     cur.close()
-    conn.close()
+    db.close()
