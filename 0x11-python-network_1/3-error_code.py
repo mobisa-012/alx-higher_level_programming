e6a0c0e20d862ababe5b89c1402700a9a610774f
Bina Mobisa <mobisakwamboka@gmail.com>
2022-09-23 19:08:59 +0300

    Python script that takes in a URL, sends a request to the URL  (decoded in utf-8).

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
+"""Sends a request to a given URL and displays the response body.
+Usage: ./3-error_code.py <URL>
+  - Handles HTTP errors.
 """
-Script that takes in a URL, sends a request to the URL and displays the body
-of the response (decoded in utf-8).
-"""
-import urllib.request
 import sys
+import urllib.error
+import urllib.request
 
 
 if __name__ == "__main__":
-    request = urllib.request.Request(sys.argv[1])
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
     try:
         with urllib.request.urlopen(request) as response:
-            print(response.read().decode('utf8'))
-    except urllib.error.HTTPError as error:
-        print('Error code: {}'.format(error.code))
+            print(response.read().decode("ascii"))
+    except urllib.error.HTTPError as e:
+            print("Error code: {}".format(e.code))
