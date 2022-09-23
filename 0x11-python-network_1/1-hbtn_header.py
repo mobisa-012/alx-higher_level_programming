d99a7788b1d2e4bb2c315648cdedefdf76bf32b4
Bina Mobisa <mobisakwamboka@gmail.com>
2022-09-23 19:01:54 +0300

    Python script that takes in a URL, sends a request to the URL

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-'''
-script that takes in a URL, sends a request to the URL and displays
-#the value of the X-Request-Id variable found in the header of the response.
-'''
+"""takes URL, sends request to URL and display value of the X-Request-Id"""
 
-
-import urllib.request
 import sys
-
+import urllib.request
 
 if __name__ == '__main__':
-        with urllib.request.urlopen(sys.argv[1]) as response:
-            print(response.info()['X-Request-Id'])
+
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        print(dict(response.headers).get("X-Request-Id"))
