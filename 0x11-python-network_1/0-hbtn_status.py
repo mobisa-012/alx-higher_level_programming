1f451a73ca83fe4dd4cb0997cd186202118e0fd0
Bina Mobisa <mobisakwamboka@gmail.com>
2022-09-23 18:53:57 +0300

    a Python script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
-"""Fetches the URL: https://intranet.hbtn.io/status
+""" This module fetches https://alx-intranet.hbtn.io/status
 """
 
-from urllib.request import Request, urlopen
+import urllib.request
+
+
+def get_alx_status():
+    """ This function fetches https://alx-intranet.hbtn.io/status
+    """
+    url = "https://alx-intranet.hbtn.io/status"
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as resp:
+        html = resp.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
 
 
 if __name__ == "__main__":
-    req = Request('https://intranet.hbtn.io/status')
-
-    with urlopen(req) as res:
-        content = res.read()
-        utf8_content = content.decode('utf-8')
-
-        print('Body response:')
-        print('\t- type: {_type}'.format(_type=type(content)))
-        print('\t- content: {_content}'.format(_content=content))
-        print('\t- utf8 content: {_utf8_c}'.format(_utf8_c=utf8_content))
+    get_alx_status()
